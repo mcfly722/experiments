5f829b5ca6e6c52b8d59467a953aa428e179a6a0
Anonymous <7221798@gmail.com>
2020-01-11 23:14:56 +0300

    URL request midlware logger

--- go-rest/internal/app/apiserver/apiserver.go
+++ go-rest/internal/app/apiserver/apiserver.go
@@ -3,6 +3,7 @@ package apiserver
 import (
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/mcfly722/experiments/go-rest/internal/app/store"
@@ -55,7 +56,17 @@ func (s *APIServer) configureLogger() error {
 }
 
 func (s *APIServer) configureRouter() {
+	s.router.Use(s.logRequest)
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		s.logger.Infof("started: %s %s", r.Method, r.RequestURI)
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		s.logger.Infof("completed in: %vms", time.Now().Sub(start).Milliseconds())
+	})
 }
 
 func (s *APIServer) configureStore() error {
