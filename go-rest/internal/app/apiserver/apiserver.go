fbeaecb8119be8e381dc58be870b00022a915242
Anonymous <7221798@gmail.com>
2020-01-15 01:40:59 +0300

    jwt token works

--- go-rest/internal/app/apiserver/apiserver.go
+++ go-rest/internal/app/apiserver/apiserver.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/mcfly722/experiments/go-rest/internal/app/store"
 	"github.com/sirupsen/logrus"
@@ -58,6 +59,36 @@ func (s *APIServer) configureLogger() error {
 func (s *APIServer) configureRouter() {
 	s.router.Use(s.logRequest)
 	s.router.HandleFunc("/hello", s.handleHello())
+	s.router.Handle("/", http.FileServer(http.Dir("./frontend/")))
+
+	var authorizationHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		if username == "hello" && password == "world" {
+			token := jwt.New(jwt.SigningMethodHS256)
+			claims := token.Claims.(jwt.MapClaims)
+			claims["userId"] = 1234
+			claims["timeStamp"] = time.Now().Unix()
+			if s.config.JWTPrivateKey == "" {
+				s.logger.Error("JWT Token error: token is not specified")
+				http.Error(w, "Unauthorized. See server log for detailed error.", http.StatusUnauthorized)
+			} else {
+				tokenString, err := token.SignedString([]byte(s.config.JWTPrivateKey))
+				if err != nil {
+					s.logger.Error("JWT Token error:", err)
+					http.Error(w, "Unauthorized. See server log for detailed error.", http.StatusUnauthorized)
+				} else {
+					w.Write([]byte(tokenString))
+				}
+			}
+		} else {
+			http.Error(w, "Unauthorized.", http.StatusUnauthorized)
+		}
+	})
+
+	s.router.Handle("/authorization", authorizationHandler).Methods("GET")
+
 }
 
 func (s *APIServer) logRequest(next http.Handler) http.Handler {
--- go-rest/internal/app/apiserver/config.go
+++ go-rest/internal/app/apiserver/config.go
@@ -4,17 +4,19 @@ import "github.com/mcfly722/experiments/go-rest/internal/app/store"
 
 // Config ...
 type Config struct {
-	BindAddr string `toml:"bind_addr"`
-	LogLevel string `toml:"log_level"`
-	Store    *store.Config
+	BindAddr      string `toml:"bind_addr"`
+	LogLevel      string `toml:"log_level"`
+	JWTPrivateKey string `toml:"jwt_private_key"`
+	Store         *store.Config
 }
 
 //NewConfig ...
 func NewConfig() *Config {
 	return &Config{
-		BindAddr: ":8080",
-		LogLevel: "debug",
-		Store:    store.NewConfig(),
+		BindAddr:      ":8080",
+		LogLevel:      "debug",
+		Store:         store.NewConfig(),
+		JWTPrivateKey: "",
 	}
 
 }
